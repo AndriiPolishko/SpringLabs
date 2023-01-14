04ecde0d30b6b726a5654245a35ac05d2b52e0df
AndriiPolishko <ramp8a799@gmail.com>
2023-01-14 21:27:38 +0200

    Added user entity and dao class

---
+++ Lab5/Lab5_JdbcTemplate/src/main/java/com/example/lab5_jdbctemplate/entity/User.java
@@ -0,0 +1,7 @@
+package com.example.lab5_jdbctemplate.entity;
+
+public class User {
+    private String email;
+    private String firstName;
+    private String secondName;
+}
---
+++ Lab5/Lab5_JdbcTemplate/src/test/java/com/example/lab5_jdbctemplate/Lab5JdbcTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab5_jdbctemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab5JdbcTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
